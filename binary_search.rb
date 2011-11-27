b0daf4e35f6e2c0cea362c8a3c48d5d225e18829
Greg Molnar <molnargerg@gmail.com>
2011-11-27 12:39:20 +0000

    wrote a search method. fixed typecast problem

--- binary_search.rb
+++ binary_search.rb
@@ -1,4 +1,18 @@
 class Array
   def binary_search(needle)
-  end  
+    # clone the array for slice
+    array = self.clone
+    # loop and slice the array on the needed side until only one element left
+    while true do 
+      size = array.size
+      break if(size == 1)
+      middle = size / 2
+      if(needle < array[middle])
+        array.slice!(middle,length)
+      else
+        array.slice!(0,middle)
+      end
+    end
+    return (array[0] == needle) ? needle : nil
+  end
 end
--- features/step_definitions/binary_search_steps.rb
+++ features/step_definitions/binary_search_steps.rb
@@ -8,5 +8,7 @@ When /^I try to find the "([^"]*)"$/ do |arg1|
 end
 
 Then /^I should get "([^"]*)"$/ do |arg1|
+  # typecast nil string to nil. 
+  arg1 = nil if arg1 == 'nil'
   @r.should == arg1
 end
