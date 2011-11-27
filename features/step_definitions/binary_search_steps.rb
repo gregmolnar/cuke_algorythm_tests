ee5627791cd40209c56490f8bde3e9f3fda6b6cb
Greg Molnar <molnargerg@gmail.com>
2011-11-27 12:30:16 +0000

    added some steps but they fail because lack binary_search method, so I need to implement that.

---
+++ features/step_definitions/binary_search_steps.rb
@@ -0,0 +1,11 @@
+Given /^an array of positive integer numbers "([^"]*)"$/ do |arg1|
+  @array = arg1.split
+end
+
+When /^I try to find the "([^"]*)"$/ do |arg1|
+  @result = @array.binary_search(arg1)
+end
+
+Then /^I should get "([^"]*)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
